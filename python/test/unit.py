6eec72ab582018a6772774e472bcace5bfda7ea9
Mihai Galos <mihaigalos@gmail.com>
2020-03-31 12:33:42 +0200

    Add bazelized template for python

---
+++ python/test/unit.py
@@ -0,0 +1,13 @@
+import unittest
+import os.path
+
+from example import Example
+
+
+class PathTest(unittest.TestCase):
+    def test_whenTypical(self):
+        self.assertTrue(os.path.isfile("test/input_data.txt") == True)
+
+
+if __name__ == '__main__':
+    unittest.main()
