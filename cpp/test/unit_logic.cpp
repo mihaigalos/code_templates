aa148993c62aaf58ba2c57e3b93ad03059de2c2c
Mihai Galos <mihaigalos@gmail.com>
2020-06-04 17:08:43 +0200

    Respect AAA

--- cpp/test/unit_logic.cpp
+++ cpp/test/unit_logic.cpp
@@ -13,8 +13,9 @@ protected:
 };
 
 TEST_F(Fixture, StateInactive_whenInitialization) {
-  auto actual_meaning_of_life = sut_.GetAnswerToLife();
   auto expected_meaning_of_life = 42;
+
+  auto actual_meaning_of_life = sut_.GetAnswerToLife();
 
   EXPECT_EQ(actual_meaning_of_life, expected_meaning_of_life);
 }
